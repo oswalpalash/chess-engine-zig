d83f67f59f4048015b4ed41446ac26931524722e
Palash Oswal <hello@oswalpalash.com>
2024-04-16 09:22:53 -0400

    Add basic eval functions

--- board.zig
+++ board.zig
@@ -11,34 +11,22 @@ const Piece = struct {
     value: u8,
     representation: u8,
     current: u64 = 0,
-
-    pub fn reverse(self: Piece) Piece {
-        var result: u64 = 0;
-        var i: u8 = 0;
-        while (i < BoardSize) : (i += 1) {
-            result |= ((self >> i) & 1) << (BoardSize - 1 - i);
-        }
-        return result;
-    }
+    stdval: u8 = 0,
 };
 
-const WhiteKing: Piece = Piece{
-    .color = 0,
-    .value = 255,
-    .representation = 'K',
-};
-const WhiteQueen: Piece = Piece{ .color = 0, .value = 9, .representation = 'Q' };
-const WhiteRook: Piece = Piece{ .color = 0, .value = 5, .representation = 'R' };
-const WhiteBishop: Piece = Piece{ .color = 0, .value = 3, .representation = 'B' };
-const WhiteKnight: Piece = Piece{ .color = 0, .value = 3, .representation = 'N' };
-const WhitePawn: Piece = Piece{ .color = 0, .value = 1, .representation = 'P' };
-const BlackKing: Piece = Piece{ .color = 1, .value = 255, .representation = 'k' };
-const BlackQueen: Piece = Piece{ .color = 1, .value = 9, .representation = 'q' };
-const BlackRook: Piece = Piece{ .color = 1, .value = 5, .representation = 'r' };
-const BlackBishop: Piece = Piece{ .color = 1, .value = 3, .representation = 'b' };
-const BlackKnight: Piece = Piece{ .color = 1, .value = 3, .representation = 'n' };
-const BlackPawn: Piece = Piece{ .color = 1, .value = 1, .representation = 'p' };
-const Empty: Piece = Piece{ .color = 2, .value = 0, .representation = '.' };
+const WhiteKing: Piece = Piece{ .color = 0, .value = 255, .representation = 'K', .stdval = 255 };
+const WhiteQueen: Piece = Piece{ .color = 0, .value = 9, .representation = 'Q', .stdval = 9 };
+const WhiteRook: Piece = Piece{ .color = 0, .value = 5, .representation = 'R', .stdval = 5 };
+const WhiteBishop: Piece = Piece{ .color = 0, .value = 3, .representation = 'B', .stdval = 3 };
+const WhiteKnight: Piece = Piece{ .color = 0, .value = 3, .representation = 'N', .stdval = 3 };
+const WhitePawn: Piece = Piece{ .color = 0, .value = 1, .representation = 'P', .stdval = 1 };
+const BlackKing: Piece = Piece{ .color = 1, .value = 255, .representation = 'k', .stdval = 255 };
+const BlackQueen: Piece = Piece{ .color = 1, .value = 9, .representation = 'q', .stdval = 9 };
+const BlackRook: Piece = Piece{ .color = 1, .value = 5, .representation = 'r', .stdval = 5 };
+const BlackBishop: Piece = Piece{ .color = 1, .value = 3, .representation = 'b', .stdval = 3 };
+const BlackKnight: Piece = Piece{ .color = 1, .value = 3, .representation = 'n', .stdval = 3 };
+const BlackPawn: Piece = Piece{ .color = 1, .value = 1, .representation = 'p', .stdval = 1 };
+const Empty: Piece = Piece{ .color = 2, .value = 0, .representation = '.', .stdval = 0 };
 
 const Position = struct {
     WhiteKing: u64,
@@ -173,11 +161,98 @@ const Position = struct {
 // board is a structure of Position
 const Board = struct {
     position: Position,
+    move_count: u32 = 0,
+    whitepieces: WhitePieces = WhitePieces{ .King = WhiteKing, .Queen = WhiteQueen, .Rook = WhiteRook, .Bishop = WhiteBishop, .Knight = WhiteKnight, .Pawn = WhitePawn },
+    blackpieces: BlackPieces = BlackPieces{ .King = BlackKing, .Queen = BlackQueen, .Rook = BlackRook, .Bishop = BlackBishop, .Knight = BlackKnight, .Pawn = BlackPawn },
 
     pub fn print(self: Board) [BoardSize]u8 {
         return self.position.print();
     }
 };
+
+const WhitePieces = struct {
+    King: Piece = WhiteKing,
+    Queen: Piece = WhiteQueen,
+    Rook: Piece = WhiteRook,
+    Bishop: Piece = WhiteBishop,
+    Knight: Piece = WhiteKnight,
+    Pawn: Piece = WhitePawn,
+
+    pub fn boardvalue(self: WhitePieces) u32 {
+        var value: u32 = 0;
+        value += self.King.stdval;
+        value += self.Queen.stdval;
+        value += self.Rook.stdval;
+        value += self.Bishop.stdval;
+        value += self.Knight.stdval;
+        value += self.Pawn.stdval;
+        return value;
+    }
+
+    pub fn currentValues(self: WhitePieces, position: Position) u32 {
+        var value: u32 = 0;
+        value = @popCount(position.WhiteKing) * self.King.value;
+        value += @popCount(position.WhiteQueen) * self.Queen.value;
+        value += @popCount(position.WhiteRook) * self.Rook.value;
+        value += @popCount(position.WhiteBishop) * self.Bishop.value;
+        value += @popCount(position.WhiteKnight) * self.Knight.value;
+        value += @popCount(position.WhitePawn) * self.Pawn.value;
+        return value;
+    }
+};
+
+const BlackPieces = struct {
+    King: Piece = BlackKing,
+    Queen: Piece = BlackQueen,
+    Rook: Piece = BlackRook,
+    Bishop: Piece = BlackBishop,
+    Knight: Piece = BlackKnight,
+    Pawn: Piece = BlackPawn,
+
+    pub fn boardvalue(self: BlackPieces) u32 {
+        var value: u32 = 0;
+        value += self.King.stdval;
+        value += self.Queen.stdval;
+        value += self.Rook.stdval;
+        value += self.Bishop.stdval;
+        value += self.Knight.stdval;
+        value += self.Pawn.stdval;
+        return value;
+    }
+
+    pub fn currentValues(self: BlackPieces, position: Position) u32 {
+        var value: u32 = 0;
+        // determine number of pieces on the board
+        value = @popCount(position.BlackKing) * self.King.value;
+        value += @popCount(position.BlackQueen) * self.Queen.value;
+        value += @popCount(position.BlackRook) * self.Rook.value;
+        value += @popCount(position.BlackBishop) * self.Bishop.value;
+        value += @popCount(position.BlackKnight) * self.Knight.value;
+        value += @popCount(position.BlackPawn) * self.Pawn.value;
+        return value;
+    }
+};
+
+pub fn basicEval(board: Board) i64 {
+    var whiteValue: u32 = board.whitepieces.boardvalue();
+    var blackValue: u32 = board.blackpieces.boardvalue();
+    var value: i64 = 0;
+    value += @as(i64, whiteValue);
+    value -= @as(i64, blackValue);
+    return value;
+}
+
+pub fn CurrentEval(board: Board) i64 {
+    var whiteValue: u32 = board.whitepieces.currentValues(board.position);
+    var blackValue: u32 = board.blackpieces.currentValues(board.position);
+    std.debug.print("White: {d}\n", .{whiteValue});
+    std.debug.print("Black: {d}\n", .{blackValue});
+    var value: i64 = 0;
+    value += @as(i64, whiteValue);
+    value -= @as(i64, blackValue);
+    std.debug.print("Value: {d}\n", .{value});
+    return value;
+}
 
 pub fn reverse(self: u64) u64 { // inverts from the center
     var result: u64 = 0;
@@ -224,12 +299,6 @@ pub fn parseFen(fen: []const u8) Position {
                 }
                 index += 1;
             },
-            '/' => {
-                // Advance to the next row, ensuring it does not wrap around to a new index out of bounds
-                // redo logic so that everything fits with u6
-                //std.debug.print("Index: {d}\n", .{index});
-                //std.debug.print("Modified Index: {d}\n", .{index});
-            },
             '1' => index += 1,
             '2' => index += 2,
             '3' => index += 3,
@@ -238,14 +307,10 @@ pub fn parseFen(fen: []const u8) Position {
             '6' => index += 6,
             '7' => index += 7,
             '8' => index += 8,
-            else => {
-                // Handle unexpected characters or spaces gracefully
-            },
+            else => {},
         }
-        i += 1; // Move to the next character
+        i += 1;
     }
-    // ensure rows are inverted
-
     return board.position.invert();
 }
 
@@ -257,4 +322,25 @@ test "print board" {
 test "fen" {
     var board: Board = Board{ .position = parseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") };
     try std.testing.expectEqualStrings(&board.print(), "RNBKQBNRPPPPPPPP................................pppppppprnbkqbnr"[0..BoardSize]);
+}
+
+test "eval of starting position" {
+    var board: Board = Board{ .position = Position.init() };
+    try std.testing.expectEqual(basicEval(board), 0);
+}
+
+test "current eval of starting position" {
+    var board: Board = Board{ .position = Position.init() };
+    try std.testing.expectEqual(CurrentEval(board), 0);
+}
+
+test "current eval of only white pieces" {
+    var board: Board = Board{ .position = Position.init() };
+    board.position.BlackKing = 0;
+    board.position.BlackQueen = 0;
+    board.position.BlackRook = 0;
+    board.position.BlackBishop = 0;
+    board.position.BlackKnight = 0;
+    board.position.BlackPawn = 0;
+    try std.testing.expectEqual(CurrentEval(board), 294);
 }
